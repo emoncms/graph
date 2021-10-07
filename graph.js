ff095770a7e537426648c7ac6bbbd0119d7dbb2b
chaveiro <nchaveiro@gmail.com>
2021-10-07 10:20:39 +0100

    Fix security, menu and support large screens
    - Fix security, only allow if user is reader or writer
    - Fix menu integration, only show if user is logeed
    - Allow large screens expand

--- graph.js
+++ graph.js
@@ -227,18 +227,20 @@ function graph_init_editor()
     // ---------------------------------------------------------------
     // Writting direct to the menu system here
     // ---------------------------------------------------------------
-    // 1. Populate custom l3 menu from sidebar html placed in hidden element
-    $(".menu-l3").html($("#sidebar_html").html());
-    // 2. Clear original hidden element
-    $("#sidebar_html").html("");
-    // 3. Populate with feed list selector
-    $("#feeds").html(out);
-    // 4. Show l3 menu
-    if (menu.width>=576) menu.show_l3();
-    // 5. Enable l3 menu so that collapsing and re-expanding works
     if (menu.obj.setup!=undefined) {
-        menu.obj.setup.l2.graph.l3 = []
-        menu.active_l3 = true;
+        if (menu.obj.setup.l2.graph != undefined) {
+            // 1. Populate custom l3 menu from sidebar html placed in hidden element
+            $(".menu-l3").html($("#sidebar_html").html());
+            // 2. Clear original hidden element
+            $("#sidebar_html").html("");
+            // 3. Populate with feed list selector
+            $("#feeds").html(out);
+            // 4. Show l3 menu
+            if (menu.width>=576) menu.show_l3();
+            // 5. Enable l3 menu so that collapsing and re-expanding works
+            menu.obj.setup.l2.graph.l3 = []
+            menu.active_l3 = true;
+        }
     }
     load_saved_graphs_menu();
     // ---------------------------------------------------------------
@@ -1841,7 +1843,7 @@ function printdate(timestamp)
     if (secs<10) secs = "0"+secs;
 
     var datestr = "";
-    //	date.getHours()+":"+minutes+" "+day+" "+month;
+    //  date.getHours()+":"+minutes+" "+day+" "+month;
     datestr += day+"/"+month;
     if (thisyear!=year) datestr += "/"+year;
     datestr += " " + date.getHours()+":"+minutes+":"+secs;
--- graph_menu.php
+++ graph_menu.php
@@ -1,12 +1,13 @@
 <?php
 global $session;
+if ($session["read"]) {
 // Initial graph menu item placement
-$menu["setup"]["l2"]['graph'] = array(
-    "name"=>_("Graphs"),
-    "href"=>"graph",
-    "order"=>3, 
-    "icon"=>"show_chart"
-);
-
+	$menu["setup"]["l2"]['graph'] = array(
+		"name"=>_("Graphs"),
+		"href"=>"graph",
+		"order"=>3, 
+		"icon"=>"show_chart"
+	);
+}
 // Full level3 sidebar is added via javascript in graph.js
 
