ac8386be829d63508777cf21cd1fd0bf2af65cb3
chaveiro <nchaveiro@gmail.com>
2021-10-07 10:25:51 +0100

    Update graph_menu.php

--- graph_menu.php
+++ graph_menu.php
@@ -2,12 +2,11 @@
 global $session;
 if ($session["read"]) {
 // Initial graph menu item placement
-	$menu["setup"]["l2"]['graph'] = array(
-		"name"=>_("Graphs"),
-		"href"=>"graph",
-		"order"=>3, 
-		"icon"=>"show_chart"
-	);
+    $menu["setup"]["l2"]['graph'] = array(
+        "name"=>_("Graphs"),
+        "href"=>"graph",
+        "order"=>3, 
+        "icon"=>"show_chart"
+    );
 }
 // Full level3 sidebar is added via javascript in graph.js
-
