4d6cec75219399d0646d80877a9d82697fc9feb1
TrystanLea <trystan.lea@gmail.com>
2025-09-09 18:25:53 +0100

    restrict access

--- group_view.php
+++ group_view.php
@@ -1,4 +1,5 @@
 <?php
+defined('EMONCMS_EXEC') or die('Restricted access');
     /*
     All Emoncms code is released under the GNU Affero General Public License.
     See COPYRIGHT.txt and LICENSE.txt.
